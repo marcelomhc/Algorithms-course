584990ce92adc234331f8b85a403e9667c1eb72b
Marcelo <mmhcastro@gmail.com>
2018-10-27 10:12:11 +0200

    Method bodies

--- WordNet/src/Outcast.java
+++ WordNet/src/Outcast.java
@@ -1,5 +1,19 @@
 public class Outcast {
-    public Outcast(WordNet wordnet)         // constructor takes a WordNet object
-    public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
-    public static void main(String[] args)  // see test client below
+
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+
+        return null;
+    }
+
+    // see test client below
+    public static void main(String[] args) {
+
+    }
+
 }
